4906888d995fa431c85b937f2921429dda1dc25c
KonstantinDobrovolsky <k.dobrovolsky@gmail.com>
2025-02-27 13:10:28 +0500

    Cyberpunk theme swither template

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,35 @@
 import { Link } from "react-router-dom";
 import 'app/styles/index.css';
 import { AppRouter } from "app/providers/appRouter";
+import Button from "shared/ui/Button/Button";
+import { useState } from "react";
+import classes from './App.module.css';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const changeTheme = () => {
+    if (theme === 'light')
+      setTheme('cyberpunk')
+    else
+      setTheme('light')
+  }
+
   return (
-    <div className="main cyberpunk">
+    <div className={`app ${theme}`}>
 
-    <div>
-      <Link className="link" to={'/playground'}>Площадка</Link>
-      <Link className="link" to={'/currentFights'}>Список боев</Link>
-      <Link className="link" to={'/tournamentResults'}>Итоги</Link>
-      <Link className="link" to={'/tournamentSettings'}>Управление турниром</Link>
+      <div className={classes.header}>
+        <div className={classes.links}>
+          <Link className={classes.link} to={'/playground'}>Площадка</Link>
+          <Link className={classes.link} to={'/currentFights'}>Список боев</Link>
+          <Link className={classes.link} to={'/tournamentResults'}>Итоги</Link>
+          <Link className={classes.link} to={'/tournamentSettings'}>Управление турниром</Link>
+        </div>
+        <Button name="" inheritClasses={classes.themeToggle} onClick={changeTheme}/>
+      </div>
+
+      <AppRouter/>
     </div>
-
-    <AppRouter/>
-  </div>
   );
 }
 
